9a34c774e9bfe476faf7bf84a6bde75ddac35600
Holger <hdecarne@gmail.com>
2017-10-15 19:30:52 +0200

    Project setup

---
+++ java-test-swt-gtk-linux-x86_64/shared-src/main/java/de/carne/test/swt/tester/SWTTester.java
@@ -0,0 +1,235 @@
+/*
+ * Copyright (c) 2017 Holger de Carne and contributors, All Rights Reserved.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package de.carne.test.swt.tester;
+
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.atomic.AtomicReference;
+import java.util.function.Supplier;
+
+import org.eclipse.swt.SWTException;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Shell;
+
+import de.carne.util.Exceptions;
+import de.carne.util.Threads;
+
+/**
+ * Base class for SWT application tests.
+ */
+public abstract class SWTTester {
+
+	private static final int SLEEP_MILLIS = 250;
+	private static final int SLEEP_COUNT_LIMIT = 20;
+
+	/**
+	 * Start and run the SWT application subject to the test.
+	 *
+	 * @param args The test command line arguments.
+	 * @see Runner#run()
+	 */
+	protected abstract void runSWTApplication(String[] args);
+
+	/**
+	 * Create a {@linkplain Runner} instance for test execution.
+	 *
+	 * @return The created {@linkplain Runner} instance.
+	 */
+	protected Runner runner() {
+		return runner(new String[0]);
+	}
+
+	/**
+	 * Create a {@linkplain Runner} instance for test execution.
+	 *
+	 * @param args The test command line arguments.
+	 * @return The created {@linkplain Runner} instance.
+	 */
+	protected Runner runner(String[] args) {
+		return new Runner(args);
+	}
+
+	private void runWait(Display display, Runnable runnable) {
+		final CountDownLatch latch = new CountDownLatch(1);
+
+		display.asyncExec(() -> {
+			try {
+				runnable.run();
+			} finally {
+				latch.countDown();
+			}
+		});
+		try {
+			latch.await();
+		} catch (InterruptedException e) {
+			Exceptions.ignore(e);
+			Thread.currentThread().interrupt();
+		}
+	}
+
+	private <T> T runWait(Display display, Supplier<T> supplier) {
+		final CountDownLatch latch = new CountDownLatch(1);
+		final AtomicReference<T> resultHolder = new AtomicReference<>();
+
+		display.asyncExec(() -> {
+			try {
+				resultHolder.set(supplier.get());
+			} finally {
+				latch.countDown();
+			}
+		});
+		try {
+			latch.await();
+		} catch (InterruptedException e) {
+			Exceptions.ignore(e);
+			Thread.currentThread().interrupt();
+		}
+		return resultHolder.get();
+	}
+
+	private int ensureSleepLimit(int sleepCount) {
+		if (sleepCount >= SLEEP_COUNT_LIMIT) {
+			throw new IllegalStateException("Sleep/wait limit reached");
+		}
+		return sleepCount + 1;
+	}
+
+	private Display waitReady(Runner runner) {
+		int sleepCount = 0;
+
+		while (Display.findDisplay(runner.displayThread()) == null) {
+			Threads.sleep(SLEEP_MILLIS);
+			sleepCount = ensureSleepLimit(sleepCount);
+		}
+
+		Display display = Display.findDisplay(runner.displayThread());
+
+		Supplier<Boolean> shellReady = () -> {
+			Shell[] shells;
+
+			return Boolean.valueOf((shells = display.getShells()).length > 0 && shells[0].isVisible());
+		};
+
+		while (!runWait(display, shellReady).booleanValue()) {
+			Threads.sleep(SLEEP_MILLIS);
+			sleepCount = ensureSleepLimit(sleepCount);
+		}
+		return display;
+	}
+
+	private void closeAllShells(Runner runner) {
+		Display display = Display.findDisplay(runner.displayThread());
+
+		if (display != null && !display.isDisposed()) {
+			for (Shell shell : display.getShells()) {
+				if (!shell.isDisposed()) {
+					shell.dispose();
+				}
+			}
+		}
+	}
+
+	private void testerThread(Runner runner) {
+		Display display = waitReady(runner);
+
+		for (Runnable check : runner) {
+			runWait(display, check);
+		}
+		runWait(display, () -> closeAllShells(runner));
+	}
+
+	void runTester(Runner runner) {
+		Thread testerThread = new Thread(() -> testerThread(runner));
+
+		testerThread.setDaemon(true);
+		testerThread.start();
+		try {
+			runSWTApplication(runner.args());
+		} catch (SWTException e) {
+			Throwable cause = e.getCause();
+
+			if (cause instanceof AssertionError) {
+				throw (AssertionError) cause;
+			}
+			throw e;
+		}
+	}
+
+	/**
+	 * Class used to setup and perform the an application test run.
+	 */
+	public class Runner implements Runnable, Iterable<Runnable> {
+
+		private final Thread displayThread;
+		private final String[] args;
+		private final List<Runnable> checks = new LinkedList<>();
+
+		Runner(String[] args) {
+			this.displayThread = Thread.currentThread();
+			this.args = args;
+		}
+
+		/**
+		 * Get the UI thread.
+		 *
+		 * @return The UI thread.
+		 */
+		public Thread displayThread() {
+			return this.displayThread;
+		}
+
+		/**
+		 * Get the test command line as submitted to {@linkplain SWTTester#runner(String[])}.
+		 *
+		 * @return The test command line.
+		 */
+		public String[] args() {
+			return this.args;
+		}
+
+		/**
+		 * Add a check to be run during application execution.
+		 *
+		 * @param check The check to add.
+		 * @return The updated {@linkplain Runner}.
+		 */
+		public Runner check(Runnable check) {
+			this.checks.add(check);
+			return this;
+		}
+
+		/**
+		 * Start and run the application and invoke the added checks.
+		 *
+		 * @see #check(Runnable)
+		 * @see SWTTester#runSWTApplication(String[])
+		 */
+		@Override
+		public void run() {
+			runTester(this);
+		}
+
+		@Override
+		public Iterator<Runnable> iterator() {
+			return this.checks.iterator();
+		}
+
+	}
+
+}
---
+++ java-test-swt-gtk-linux-x86_64/shared-src/test/java/de/carne/test/swt/tester/SWTTesterTest.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright (c) 2017 Holger de Carne and contributors, All Rights Reserved.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package de.carne.test.swt.tester;
+
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Shell;
+import org.junit.Test;
+
+/**
+ * Test {@linkplain SWTTester} class.
+ */
+public class SWTTesterTest extends SWTTester {
+
+	private static final String TITLE_SHELL = "Shell";
+
+	@Override
+	protected void runSWTApplication(String[] args) {
+		Display display = new Display();
+		Shell shell = new Shell(display);
+
+		shell.setText(TITLE_SHELL);
+		shell.open();
+		while (!display.isDisposed() && display.getShells().length > 0) {
+			if (!display.readAndDispatch()) {
+				display.sleep();
+			}
+		}
+	}
+
+	/**
+	 * Run and verify test application.
+	 */
+	@Test
+	public void testerTest() {
+		runner().run();
+	}
+
+}
