251412b9cdc684e15a7eab12b9ad4ce17290ece6
Holger <hdecarne@gmail.com>
2018-05-06 11:34:51 +0200

    Add Property related classes

---
+++ java-swt-gtk-linux-x86_64/shared-src/main/java/de/carne/swt/util/Property.java
@@ -0,0 +1,107 @@
+/*
+ * Copyright (c) 2007-2018 Holger de Carne and contributors, All Rights Reserved.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package de.carne.swt.util;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.function.Supplier;
+
+import de.carne.boot.check.Nullable;
+
+/**
+ * This class represents an observable property value.
+ *
+ * @param <T> the actual property type.
+ */
+public class Property<T> implements Supplier<T> {
+
+	@Nullable
+	private T value;
+	private List<PropertyChangedListener<T>> changedListeners = new ArrayList<>();
+
+	/**
+	 * Constructs a new {@linkplain Property} instance.
+	 * <p>
+	 * The initial property value is {@code null}.
+	 */
+	public Property() {
+		this(null);
+	}
+
+	/**
+	 * Constructs a new {@linkplain Property} instance.
+	 *
+	 * @param value the initial property value.
+	 */
+	public Property(@Nullable T value) {
+		this.value = value;
+	}
+
+	/**
+	 * Gets the property value.
+	 */
+	@Override
+	@Nullable
+	public T get() {
+		return this.value;
+	}
+
+	/**
+	 * Sets the property value.
+	 *
+	 * @param newValue the value to set.
+	 * @return the old property value.
+	 */
+	@Nullable
+	public T set(T newValue) {
+		T oldValue = this.value;
+
+		this.value = newValue;
+		if (!Objects.equals(this.value, oldValue)) {
+			for (PropertyChangedListener<T> changedListener : this.changedListeners) {
+				changedListener.changed(this.value, oldValue);
+			}
+		}
+		return oldValue;
+	}
+
+	/**
+	 * Adds a {@linkplain PropertyChangedListener}.
+	 * <p>
+	 * The added listener is called every time the property value is changed.
+	 *
+	 * @param changedListener the listener to add.
+	 * @return the updated {@linkplain Property} instance.
+	 */
+	public Property<T> addChangedListener(PropertyChangedListener<T> changedListener) {
+		this.changedListeners.add(changedListener);
+		return this;
+	}
+
+	/**
+	 * Removes a previously added {@linkplain PropertyChangedListener}.
+	 *
+	 * @param changedListener the listener to remove.
+	 * @return the updated {@linkplain Property} instance.
+	 */
+	public Property<T> removeChangedListener(PropertyChangedListener<T> changedListener) {
+		this.changedListeners.remove(changedListener);
+		return this;
+	}
+
+}
