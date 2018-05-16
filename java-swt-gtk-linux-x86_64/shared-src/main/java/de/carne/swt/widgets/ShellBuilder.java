dfcc5e9622bf0d42da9a85dc54b89b4e642675b2
Holger <hdecarne@gmail.com>
2018-05-16 22:04:05 +0200

    Add position function.

--- java-swt-gtk-linux-x86_64/shared-src/main/java/de/carne/swt/widgets/ShellBuilder.java
+++ java-swt-gtk-linux-x86_64/shared-src/main/java/de/carne/swt/widgets/ShellBuilder.java
@@ -22,12 +22,14 @@ import java.util.function.Supplier;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.ShellEvent;
 import org.eclipse.swt.graphics.Image;
+import org.eclipse.swt.graphics.Rectangle;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Decorations;
 import org.eclipse.swt.widgets.Shell;
 
 import de.carne.swt.events.EventConsumer;
 import de.carne.swt.events.EventReceiver;
+import de.carne.swt.platform.PlatformIntegration;
 
 /**
  * {@linkplain Shell} builder.
@@ -234,6 +236,73 @@ public class ShellBuilder extends CompositeBuilder<Shell> {
 
 		get().addListener(SWT.Close, listener);
 		return this;
+	}
+
+	/**
+	 * Set the {@linkplain Shell}'s position according to the given horizontal (LEFT|CENTER|RIGHT) and vertical
+	 * (TOP|CENTER|BOTTOM) alignment.
+	 * <p>
+	 * If DEFAULT is submitted as the alignment value a platform specific default is selected.
+	 *
+	 * @param horizontal the horizontal alignment to apply.
+	 * @param vertical the vertical alignment to apply.
+	 * @return the updated builder.
+	 */
+	public ShellBuilder position(int horizontal, int vertical) {
+		Shell shell = get();
+		Composite parent = shell.getParent();
+
+		if (parent != null) {
+			Rectangle shellBounds = shell.getBounds();
+			Rectangle parentBounds = parent.getBounds();
+			int shellX;
+			int shellY;
+
+			switch (getEffectiveHorizontal(horizontal)) {
+			case SWT.LEFT:
+				shellX = parentBounds.x;
+				break;
+			case SWT.RIGHT:
+				shellX = parentBounds.x + parentBounds.width - shellBounds.width;
+				break;
+			default:
+				shellX = parentBounds.x + (parentBounds.width - shellBounds.width) / 2;
+			}
+			switch (getEffectiveVertical(vertical)) {
+			case SWT.TOP:
+				shellY = parentBounds.y;
+				break;
+			case SWT.BOTTOM:
+				shellY = parentBounds.y + parentBounds.height - shellBounds.height;
+				break;
+			default:
+				shellY = parentBounds.y + (parentBounds.height - shellBounds.height) / 2;
+			}
+			shell.setLocation(shellX, shellY);
+		}
+		return this;
+	}
+
+	private int getEffectiveHorizontal(int horizontal) {
+		int effectiveHorizontal;
+
+		if (horizontal == SWT.DEFAULT) {
+			effectiveHorizontal = SWT.CENTER;
+		} else {
+			effectiveHorizontal = horizontal;
+		}
+		return effectiveHorizontal;
+	}
+
+	private int getEffectiveVertical(int vertical) {
+		int effectiveVertical;
+
+		if (vertical == SWT.DEFAULT) {
+			effectiveVertical = (PlatformIntegration.isCocoa() ? SWT.TOP : SWT.CENTER);
+		} else {
+			effectiveVertical = vertical;
+		}
+		return effectiveVertical;
 	}
 
 }
