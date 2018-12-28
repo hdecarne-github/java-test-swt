ffa05ab577cc1cc1ea803a33595a01aac5b1c3d4
Holger <hdecarne@gmail.com>
2018-12-28 23:43:26 +0100

    Enable OSX build during CI

--- .travis/linux/before_script.sh
+++ .travis/linux/before_script.sh
@@ -4,5 +4,5 @@ export DISPLAY=:99.0
 export DBUS_SESSION_BUS_ADDRESS=/dev/null
 sh -e /etc/init.d/xvfb start
 sleep 10 # give xvfb some time to start
-metacity --sm-disable --replace 2> metacity.err &"
+metacity --sm-disable --replace 2> metacity.err &
 sleep 10 # give metacity some time to start
