610d83269bfb17308d4c57d6bc6381c249867582
Holger <hdecarne@gmail.com>
2019-01-02 08:54:02 +0100

    Use fluxbox for UI tests

--- .travis/linux/before_script.sh
+++ .travis/linux/before_script.sh
@@ -1,7 +1,5 @@
 #!/bin/bash -x
 
-sh -e /etc/init.d/xvfb start
+/etc/init.d/xvfb start
 sleep 10 # give xvfb some time to start
-#metacity --sm-disable --replace 2>&1 &
-fluxbox &
-sleep 10 # give metacity some time to start
+fluxbox -display :99 &
