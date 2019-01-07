cb575ddd7009f6ed40ea8358716b4ff167ba536c
Holger <hdecarne@gmail.com>
2019-01-08 00:00:22 +0100

    Handle native dialogs gracefully

--- java-test-swt-gtk-linux-x86_64/shared-src/main/java/de/carne/test/swt/tester/SWTTest.java
+++ java-test-swt-gtk-linux-x86_64/shared-src/main/java/de/carne/test/swt/tester/SWTTest.java
@@ -22,6 +22,7 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.stream.Stream;
 
+import org.eclipse.swt.widgets.Dialog;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Shell;
 import org.junit.jupiter.api.Assertions;
