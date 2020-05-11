b7081d35a05a70d8da01c67e5c46b534d1e0716b
Holger <hdecarne@gmail.com>
2020-05-11 06:53:12 +0200

    Use import instead of gnome-screenshot for screen grabbing

--- .travis/linux/before_script.sh
+++ .travis/linux/before_script.sh
@@ -2,9 +2,5 @@
 
 fluxbox >/dev/null 2>&1 &
 sleep 20 # give programs some time to start
-gnome-screenshot -c
-gnome-screenshot -c --display=$DISPLAY
-gnome-screenshot -f screen1.png
-gnome-screenshot -f screen1.png
-gnome-screenshot -f screen2.png --display=$DISPLAY
-gnome-screenshot -f screen2.png --display=$DISPLAY
+import -window root screen1.png
+import -window root screen1.png
