c56861ccf465653031ae7ca66fc218a374dd3bb6
Holger <hdecarne@gmail.com>
2020-05-11 06:42:14 +0200

    Install gnome-screenshot for screen grabbing

--- .travis/linux/before_script.sh
+++ .travis/linux/before_script.sh
@@ -2,3 +2,9 @@
 
 fluxbox >/dev/null 2>&1 &
 sleep 20 # give programs some time to start
+gnome-screenshot -c
+gnome-screenshot -c --display=$DISPLAY
+gnome-screenshot -f screen1.png
+gnome-screenshot -f screen1.png
+gnome-screenshot -f screen2.png --display=$DISPLAY
+gnome-screenshot -f screen2.png --display=$DISPLAY
